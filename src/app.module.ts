7f6ba75bc0ac9ce991bd295ad8ba4f2b6744e0c1
Jakub Korczyk <jakubkorczyk@gmail.com>
2025-04-04 10:50:50 +0200

    Database connection added.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,31 @@
 import { Module } from '@nestjs/common';
 import { CharactersModule } from './characters/characters.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [CharactersModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        return {
+          type: 'postgres',
+          host: configService.get('DB_HOST'),
+          port: configService.get('DB_PORT'),
+          username: configService.get('DB_USER'),
+          password: configService.get('DB_PASS'),
+          database: configService.get('DB_NAME'),
+          entities: [`${__dirname}/**/*.entity.{js,ts}`],
+          synchronize: true,
+        };
+      },
+    }),
+    CharactersModule,
+  ],
   controllers: [],
   providers: [],
 })
--- src/characters/characters.module.ts
+++ src/characters/characters.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { CharactersController } from './application/characters.controller';
+import { CharactersService } from './domain/characters.service';
 
 @Module({
   controllers: [CharactersController],
+  providers: [CharactersService],
 })
 export class CharactersModule {}

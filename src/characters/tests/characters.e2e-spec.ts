b5e6f90ed7e02b5615c7250499cb082dfa98e714
Jakub Korczyk <jakubkorczyk@gmail.com>
2025-04-04 16:31:01 +0200

    Tests set up

--- src/characters/tests/characters.e2e-spec.ts
+++ src/characters/tests/characters.e2e-spec.ts
@@ -1,3 +1,7 @@
+import * as dotenv from 'dotenv';
+
+dotenv.config({ path: '.env.test' });
+
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication, ValidationPipe } from '@nestjs/common';
 import * as request from 'supertest';
